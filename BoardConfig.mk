c500b567a3e6a64832b863916c23f8aa81aacf4a
paul-xxx <thepaulxxx@gmail.com>
2014-10-16 01:20:25 +0600

    fix wlan and bt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -39,16 +39,20 @@ TARGET_BOARD_INFO_FILE := device/somc/hazard/board-info.txt
 # BT
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUEDROID_VENDOR_CONF := device/somc/hazard/bluetooth/vnd_hazard.txt
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/somc/hazard/bluetooth
 
-# Wlan
-BOARD_HAS_QCOM_WLAN := true
-BOARD_WLAN_DEVICE := bcmdhd
-WPA_SUPPLICANT_VERSION := VER_0_8_X
+# Wifi
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WLAN_DEVICE                := bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
-WIFI_DRIVER_FW_PATH_STA := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
-WIFI_DRIVER_FW_PATH_AP  := "/system/vendor/firmware/fw_bcmdhd.bin"
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_AP           := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA          := "/system/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_P2P		 := "/system/vendor/firmware/fw_bcmdhd.bin"
 
 # Display
 BOARD_EGL_CFG := device/somc/hazard/egl.cfg
