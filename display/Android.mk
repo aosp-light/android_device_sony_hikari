f9f20bd752a6d74be5d56867e30018be97e41b76
paul-xxx <thepaulxxx@gmail.com>
2014-12-10 23:06:29 +0600

    moved to fuji

--- display/Android.mk
+++
@@ -1,7 +0,0 @@
-display-hals := libgralloc libgenlock libcopybit
-display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
-display-hals += libmemtrack
-ifneq ($(filter msm8660,$(TARGET_BOARD_PLATFORM)),)
-    include $(call all-named-subdir-makefiles,$(display-hals))
-endif
-
