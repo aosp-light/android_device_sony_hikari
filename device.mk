910ae3235f7d50deb7b8e5f628b97953638ee42e
paul-xxx <thepaulxxx@gmail.com>
2014-12-17 01:53:43 +0600

    add missing things: Fix bluetooth, by building packages Fix audio, by copy media files Fix booting, adding init Fix recovery Fix FM and Moooooore

--- device.mk
+++ device.mk
@@ -24,6 +24,10 @@ DEVICE_PACKAGE_OVERLAYS += device/sony/hikari/overlay
 PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
+# Light
+PRODUCT_PACKAGES += \
+    lights.semc
+
 # Display
 PRODUCT_PACKAGES += \
     liboverlay \
@@ -31,6 +35,53 @@ PRODUCT_PACKAGES += \
     gralloc.msm8660 \
     memtrack.msm8660 \
     copybit.msm8660
+
+# Media
+PRODUCT_PACKAGES += \
+    libdivxdrmdecrypt \
+    libOmxVdec \
+    libOmxVenc \
+    libOmxCore \
+    libstagefrighthw \
+    libc2dcolorconvert
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.primary.msm8660 \
+    audio.a2dp.default \
+    audio.usb.default \
+    audio.r_submix.default \
+    libaudio-resampler
+
+PRODUCT_COPY_FILES += \
+    frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    $(LOCAL_PATH)/config/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/config/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/config/audio_policy.conf:system/etc/audio_policy.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/mixer_paths.xml:system/etc/mixer_paths.xml
+
+# NFC
+PRODUCT_PACKAGES += \
+    nfc.msm8660 \
+    libnfc \
+    libnfc_jni \
+    Nfc \
+    Tag \
+    com.android.nfc_extras
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/nfcee_access.xml:system/etc/nfcee_access.xml
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbt-vendor \
+    libbluedroid \
+    brcm_patchram_plus \
+    bt_vendor.conf
 
 # Wifi
 PRODUCT_PACKAGES += \
@@ -82,10 +133,87 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
 
+# Configuration scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/flashled_calc_parameters.cfg:system/etc/flashled_calc_parameters.cfg \
+    $(LOCAL_PATH)/config/iddd.conf:system/etc/iddd.conf \
+    $(LOCAL_PATH)/config/qosmgr_rules.xml:system/etc/qosmgr_rules.xml \
+    $(LOCAL_PATH)/config/thermald.conf:system/etc/thermald.conf
+
+# Common Qualcomm / Sony scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/logo.rle:root/logo.rle \
+    $(LOCAL_PATH)/config/init.sony.usb.rc:root/init.sony.usb.rc \
+    $(LOCAL_PATH)/config/init.qcom.sh:root/init.qcom.sh \
+    $(LOCAL_PATH)/config/init.qcom.class_core.sh:root/init.qcom.class_core.sh \
+    $(LOCAL_PATH)/config/init.qcom.class_main.sh:root/init.qcom.class_main.sh \
+    $(LOCAL_PATH)/config/init.netconfig.sh:system/etc/init.netconfig.sh \
+    $(LOCAL_PATH)/config/init.qcom.efs.sync.sh:system/etc/init.qcom.efs.sync.sh \
+    $(LOCAL_PATH)/config/init.qcom.fm.sh:system/etc/init.qcom.fm.sh \
+    $(LOCAL_PATH)/config/init.qcom.post_boot.sh:system/etc/init.qcom.post_boot.sh \
+    $(LOCAL_PATH)/config/pre_hw_config.sh:system/etc/pre_hw_config.sh \
+    $(LOCAL_PATH)/config/hw_config.sh:system/etc/hw_config.sh \
+    $(LOCAL_PATH)/config/clearpad_fwloader.sh:system/etc/clearpad_fwloader.sh
+
+# Custom init / uevent
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/init.semc.rc:root/init.semc.rc \
+    $(LOCAL_PATH)/config/init.sony.rc:root/init.sony.rc \
+    $(LOCAL_PATH)/config/ueventd.semc.rc:root/ueventd.semc.rc \
+    $(LOCAL_PATH)/config/fstab.semc:root/fstab.semc \
+    $(LOCAL_PATH)/config/init.sony-platform.rc:root/init.sony-platform.rc
+
 # BT
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/bt_vendor.conf:system/etc/bluetooth/bt_vendor.conf \
     $(LOCAL_PATH)/config/BCM4330.hcd:system/etc/firmware/BCM4330.hcd
+
+# USB
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+
+# Fm radio
+PRODUCT_PACKAGES += \
+    com.stericsson.hardware.fm \
+    com.stericsson.hardware.fm.xml \
+    FmRadio
+
+# Key layouts and touchscreen
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/config/clearpad.idc:system/usr/idc/clearpad.idc \
+    $(LOCAL_PATH)/config/clearpad.kl:system/usr/keylayout/clearpad.kl \
+    $(LOCAL_PATH)/config/fuji-keypad.kl:system/usr/keylayout/fuji-keypad.kl \
+    $(LOCAL_PATH)/config/gpio-key.kl:system/usr/keylayout/gpio-key.kl \
+    $(LOCAL_PATH)/config/keypad-pmic-fuji.kl:system/usr/keylayout/keypad-pmic-fuji.kl \
+    $(LOCAL_PATH)/config/pmic8058_pwrkey.kl:system/usr/keylayout/pmic8058_pwrkey.kl \
+    $(LOCAL_PATH)/config/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
+    $(LOCAL_PATH)/config/simple_remote_appkey.kl:system/usr/keylayout/simple_remote_appkey.kl \
+    $(LOCAL_PATH)/config/Vendor_0583_Product_A000.kl:system/usr/keylayout/Vendor_0583_Product_A000.kl
+
+# Ramdisk recovery support
+PRODUCT_COPY_FILES += \
+    device/sony/hikari/recovery/bootrec-device:recovery/root/etc/bootrec-device \
+    device/sony/hikari/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh \
+    device/sony/hikari/recovery/rebootrecovery.sh:recovery/root/sbin/rebootrecovery.sh
+
+# Software
+PRODUCT_PACKAGES += \
+    libemoji \
+    e2fsck \
+    fsck_msdos \
+    Email \
+    Stk
+
+PRODUCT_PACKAGES += \
+    busybox \
+    extract_elf_ramdisk
+
+PRODUCT_PACKAGES += \
+    FujiLayout \
+    fileman
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
