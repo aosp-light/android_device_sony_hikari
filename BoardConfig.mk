ae33d609db61a820ac53db1b6607ae758b44dda3
paul-xxx <thepaulxxx@gmail.com>
2015-11-15 07:32:46 +0600

    New changes for stable work android 5.1.x 1. Clean unneded 2. Move bluetooth 3. Enable DASH 4. Enable Wifi Display 5. Some recovery build fix 6. New kernel config build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,7 +18,6 @@ COMMON_GLOBAL_CFLAGS += -DLEGACY_BLOB_COMPATIBLE
 # Display
 USE_OPENGL_RENDERER := true
 BOARD_EGL_CFG := device/sony/hikari/config/egl.cfg
-ARCH_ARM_HAVE_NEON := true
 TARGET_USES_ION := true
 TARGET_USES_OVERLAY := true
 TARGET_USES_SF_BYBASS := true
@@ -30,11 +29,11 @@ BOARD_USES_ALSA_AUDIO := true
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUEDROID_VENDOR_CONF := device/sony/hikari/bluetooth/vnd_hikari.txt
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/hikari/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR ?= device/sony/hikari/bluetooth
 
 # Sensor
 BOARD_USES_GENERIC_INVENSENSE := false
+BOARD_USES_SENSORS_DASH := yes
 SOMC_CFG_SENSORS_ACCEL_BMA250NA_INPUT := yes
 SOMC_CFG_SENSORS_COMPASS_AK8972 := yes
 SOMC_CFG_SENSORS_LIGHT_LIBALS := yes
@@ -52,7 +51,7 @@ BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
 WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/system/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/system/vendor/firmware/fw_bcmdhd_apsta.bin"
-#WIFI_DRIVER_FW_PATH_P2P     := "/system/vendor/firmware/fw_bcmdhd_p2p.bin"
+WIFI_DRIVER_FW_PATH_P2P     := "/system/vendor/firmware/fw_bcmdhd_p2p.bin"
 
 # Camera
 BOARD_USES_CAMERA_FAST_AUTOFOCUS := true
@@ -61,6 +60,10 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 # FM Radio
 BOARD_HAVE_FMRADIO := true
 BOARD_HAVE_FMRADIO_BCM := true
+
+# kernel
+#BOARD_KERNEL_MSM := true
+#KERNEL_DEFCONFIG := fuji_hikari_defconfig
 
 # Board
 TARGET_BOARD_PLATFORM := msm8660
@@ -76,21 +79,21 @@ ifeq ($(HOST_OS),linux)
 endif
 DONT_DEXPREOPT_PREBUILTS := true
 
-BOARD_VOLD_MAX_PARTITIONS := 16
+# Blyadskaya JAVA
+FUCK_THE_JAVA := true
 
-# Image
-TARGET_USERIMAGES_USE_EXT4 := true
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
-BOARD_FLASH_BLOCK_SIZE := 131072
+# EMMC Partition info
+TARGET_BOOTLOADER_TYPE := fastboot
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_RECOVERY := false
+BOARD_HAS_NO_MISC_PARTITION := true
 
 # Recovery
-TARGET_NO_BOOTLOADER := true
-BOARD_HAS_NO_MISC_PARTITION := true
 TARGET_RECOVERY_FSTAB = device/sony/hikari/config/fstab.semc
+TARGET_NO_SEPARATE_RECOVERY := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-TARGET_NO_SEPARATE_RECOVERY := true
 BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p15
 
 # TWRP
@@ -98,34 +101,37 @@ DEVICE_RESOLUTION := 720x1280
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_INTERNAL_STORAGE_PATH := "/sdcard"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
-TW_TARGET_USES_QCOM_BSP := true
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_TARGET_USES_QCOM_BSP := true
 
 # OTA
 TARGET_RELEASETOOLS_EXTENSIONS := device/sony/hikari
 TARGET_OTA_ASSERT_DEVICE := LT26,LT26w,SO-03D,hikari
 
-# Enable dual-recovery custom ota script
-SONY_USE_CUSTOM_MKBOOTIMG := true
-SONY_USE_CUSTOM_BOOT := true
-
-ifeq ($(SONY_USE_CUSTOM_BOOT),true)
-ifeq ($(SONY_USE_CUSTOM_MKBOOTIMG),true)
-BOARD_CUSTOM_BOOTIMG_MK := device/sony/hikari/customboot.mk
-else
+# ELF Image build
+#BOARD_CUSTOM_MKBOOTIMG := device/sony/hikari/tools/fullmkelf.py
+#BOARD_MKBOOTIMG_ARGS := \
+#	device/sony/hikari-kernel/zImage@0x40208000 \
+#	out/target/product/hikari/ramdisk.img@0x41500000,ramdisk \
+#	device/sony/hikari/prebuilt/RPM.bin@0x20000,rpm
 BOARD_CUSTOM_BOOTIMG_MK := device/sony/hikari/stockboot.mk
-endif
-else
-BOARD_CUSTOM_MKBOOTIMG := device/sony/hikari/tools/mkbootimg.py
-BOARD_MKBOOTIMG_ARGS := \
-	out/target/product/hikari/kernel@0x40208000 \
-	out/target/product/hikari/ramdisk.img@0x41500000,ramdisk \
-	device/sony/hikari/prebuilt/RPM.bin@0x20000,rpm
-endif
+
+# Image
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_VOLD_MAX_PARTITIONS := 15
 
 -include vendor/sony/hikari/BoardConfigVendor.mk
 
-# Health Daemon
-BOARD_BATTERY_DEVICE_NAME := "semc_battery_data"
-BOARD_HAL_STATIC_LIBRARIES := libhealthd.semc
+# SD Mount
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+BOARD_UMS_LUNFILE := "/sys/devices/platform/msm_hsusb/gadget/lun%d/file"
+
+# Enable Minikin text layout engine (will be the default soon)
+USE_MINIKIN := true
+
+# Include an expanded selection of fonts
+EXTENDED_FONT_FOOTPRINT := true
